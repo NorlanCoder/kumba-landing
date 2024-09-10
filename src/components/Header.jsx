7b2807c51a645270f08d9af314f9763566ea2d0a
Darking <zsamuel737@gmail.com>
2024-09-10 06:26:22 +0100

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,21 @@ import img1 from '../img-phone-1.png'
 // navbar component
 function Navbar(){
     return <>
-        <div className="navbar w-full mt-6 flex items-center justify-around">
+        <div className="navbar w-full mt-6 flex items-center md:justify-around sm:px-8 justify-between">
             <div className="logo z-20">
-                <h1 className="text-4xl font-bold">KUMBA</h1>
+                <a href="#"><h1 className="text-4xl text-terre font-bold">KUMBA</h1></a>
             </div>
-            <div className="menu z-20  mt-2">
-                <ul className="flex font-extralight gap-x-8">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Statistiques</li>
-                    <li>Solutions</li>
+            <div className="menu z-20 mt-2">
+                <ul className="md:flex hidden font-extralight gap-x-8">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Statistiques</a></li>
+                    <li><a href="#">Solutions</a></li>
                 </ul>
+                <div className='md:hidden flex text-terre w-16'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></div>
             </div>
-            <div className="download-button z-20">
-                <a href="#" className="h-12 px-5 py-3 text-md font-md mx-auto rounded-lg bg-[#e58d66] text-white">Télécharger</a>
+            <div className="download-button md:flex hidden z-20">
+                <a href="#" className="md:flex hidden h-12 px-5 py-3 text-md font-md mx-auto rounded-lg bg-terre text-white">Télécharger</a>
             </div>
         </div>
     </>
@@ -32,21 +33,21 @@ function Header(){
         
         <div className="w-full h-[650px] -mt-16 bg-white flex">
             <div className="flex lg:justify-between sm:justify-center px-10 mt-24">
-                <div className="text-content lg:w-[50%] z-10 mt-20 ml-24">
+                <div className="text-content lg:w-[50%] z-10 mt-20 lg:ml-24 ml-12">
                     <h1 className="text-[60px] font-[500] antialiased text-[#23273a] leading-[50px]">Drive without
                     <br/> <span className="text-[#23273a]">worries,trust us</span></h1>
                     
                     <h1 className='text-xl font-normal text-[#23273a] tracking-normal mt-12 my-4 lg:mr-[5%] sm:mr-0'>Discover the freedom of getting around hassle-free with our TripShare app. 
                         Simplify your life and discover a new way to get around.
                     </h1>
-                    <div className="buttons flex mx-5 lg:mt-[10%] mt-4 gap-x-4">
-                        <div className="download inline rounded-xl w-[154px] text-white text-center py-2 bg-[#8095f4] text-sm font-medium">Télecharger</div>
-                        <div className="download inline rounded-xl  w-[154px] h-[36px] text-center py-2 bg-[#d6defe] text-[#23273a] text-sm font-medium">Lire plus</div>
+                    <div className="buttons flex sm:justify-center lg:justify-normal mx-5 mt-[10%] gap-x-4">
+                        <div className="download inline rounded-xl w-[154px] text-terre border-1 border-terre text-center py-2 bg-[#e5e7e6] text-sm font-medium">Télecharger</div>
+                        <div className="download inline rounded-xl  w-[154px] h-[36px] text-center py-2 bg-[#B67332] text-white text-sm font-medium">Lire plus</div>
                     </div>
 
-                    <div className="stats flex items-center mt-[10%]">
+                    <div className="stats flex items-center lg:justify-normal sm:justify-center mt-[10%]">
                         <h1 className="number text-3xl font-bold">3,875</h1>
-                        <p className="text text-xs ml-2 font-light leading-3 ">people have downloaded the
+                        <p className="text text-xs md:ml-2 font-light leading-3 ">people have downloaded the
                         <span className='block'>application this month</span>  </p>
                     </div>
                 </div>
