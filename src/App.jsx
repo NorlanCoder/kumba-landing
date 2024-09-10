facd4bcf2fa0b7d3468ba307527287d9db3e77df
AxelSalim <adjakidjememiaghe@gmail.com>
2024-09-10 19:10:27 +0100

    modification du fichier principale

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import  './App.css';
+import About from './pages/About.jsx'
+import Accordion from './components/Accordion.jsx';
 import Footer from './components/Footer.jsx';
+import Solution from './components/Solution.jsx';
+import Partner from './components/Partner.jsx';
+import Statistical from './components/Statistical.jsx';
 
 function App() {
   return (
     <div className="App">
+
+      <Statistical />
+      <Partner />
+      <Solution />
+      <About />
+      <Accordion />
       <Footer/>
     </div>
   );
