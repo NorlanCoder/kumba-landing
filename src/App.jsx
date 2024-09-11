cadd29cb7588a0de1fbee25dd3c08ebac911a298
HadiCodeMaster <hadicodemaster@gmail.com>
2024-09-11 17:10:48 +0200

    implementation du footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import  './App.css';
-import About from './pages/About.jsx'
+import AboutUs from './components/AboutUs.jsx'
 import Accordion from './components/Accordion.jsx';
 import Footer from './components/Footer.jsx';
 import Solution from './components/Solution.jsx';
 import Partner from './components/Partner.jsx';
 import Statistical from './components/Statistical.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
       <Statistical />
+      <AboutUs />
       <Partner />
       <Solution />
-      <About />
       <Accordion />
       <Footer/>
     </div>
