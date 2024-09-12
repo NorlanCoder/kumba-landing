1c3cb959dfe8e45dd8f23fac550725fab4f6c71a
Darking <zsamuel737@gmail.com>
2024-09-12 13:38:04 +0100

    fix text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './css/style.css'
 import img1 from '../img-phone-1.png' 
+import logo_kumba from '../assets/img/LOGO_KUMBA_AUDACE-01.webp'
 
 
 // navbar component
@@ -26,8 +27,8 @@ function Navbar(){
     return <>
             <div className={menu_class}>
                 <div className="content mt-8">
-                    <div className='logo text-center mb-6'>
-                        <a href="#"><h1 className="md:text-4xl text-terre font-bold">KUMA</h1></a>
+                    <div className='logo flex justify-center mb-6'>
+                        <a href="#"><img className='w-16' src={logo_kumba} alt="" srcset="" /></a>
                     </div>
                     <div className="md:text-xl rounded-lg flex flex-col justify-center gap-y-4 font-extralight">
                         <a href="#" className='border-t-2 w-full text-center border-terre py-3'><span className='mx-4'>Home</span></a>
@@ -39,7 +40,7 @@ function Navbar(){
             </div>
             <div className="navbar w-full md:pt-6 pt-2 flex items-center md:justify-around md:bg-white bg-[#e5e7e6] md:px-12 px-6 justify-between">
                 <div className="logo z-10 mt-2">
-                    <a href="#"><h1 className="md:text-4xl text-2xl text-terre font-bold">KUMBA</h1></a>
+                    <a href="#"><img className='w-20' src={logo_kumba} alt="" srcset="" /></a>
                 </div>
                 <div className="menu z-20 mt-2">
                     <ul className="md:flex hidden font-extralight gap-x-8">
@@ -76,11 +77,10 @@ function Header(){
         <div className="w-full md:h-[650px] -mt-20 md:flex md:bg-white bg-[#e5e7e6]">
             <div className="flex lg:justify-between justify-center px-10 md:mt-24 mt-12">
                 <div className="text-content lg:w-[50%] z-10 md:mt-20 mt-16 lg:ml-24 sm:ml-12">
-                    <h1 className="md:text-[60px] text-4xl font-[500] antialiased text-[#23273a] md:text-left text-center leading-[50px]">Drive without
-                    <br/> <span className="text-[#23273a]">worries,trust us</span></h1>
+                    <h1 className="md:text-[60px] text-4xl font-[500] antialiased text-[#23273a] md:text-left text-center md:leading-[60px]">Logement rapide,
+                    <br/> <span className="text-[#23273a]">simple, sûr</span></h1>
                     
-                    <h1 className='md:text-xl text-md font-normal text-[#23273a] tracking-normal md:text-left text-center md:mt-12 mt-6 sm:mr-0'>Discover the freedom of getting around hassle-free with our TripShare app. 
-                        Simplify your life and discover a new way to get around.
+                    <h1 className='md:text-xl text-md font-normal text-[#23273a] tracking-normal md:text-left text-center md:mt-12 mt-6 sm:mr-0'>Avec Kumba, explorez une vaste sélection de logements adaptés à vos besoins, avec des critères clairs et une interface intuitive. Simple, rapide, et toujours à votre service.
                     </h1>
                     <div className="buttons flex justify-center lg:justify-normal md:mt-[10%] mt-4 gap-x-4">
                         <a href="#" className='download inline rounded-xl w-[154px] text-terre border-2 border-terre text-center py-2 bg-[#e5e7e6] text-sm font-medium'>Télecharger</a>
@@ -89,8 +89,8 @@ function Header(){
 
                     <div className="stats flex items-center lg:justify-normal justify-center md:mt-[10%] my-5">
                         <h1 className="number md:text-3xl text-xl font-bold">3,875</h1>
-                        <p className="text md:text-xs text-[10px] md:ml-2 font-light md:leading-3 leading-[9px]">people have downloaded the
-                        <span className='block'>application this month</span>  </p>
+                        <p className="text md:text-xs text-[10px] md:ml-2 font-light md:leading-3 leading-[9px]">personne ont télechargé
+                        <span className='block'>Kumba ce mois</span>  </p>
                     </div>
                 </div>
                 <div className="img lg:flex hidden z-10 lg:w-[45%] mr-[8%] px-8 justify-center items-center mt-0">
